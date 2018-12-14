a720d3e40df9b6e9ca31c99ad43c2255896c8934
Lucy <n0975116268@gmail.com>
2018-12-14 15:19:37 +0800

    add count

--- guess-num.py
+++ guess-num.py
@@ -1,13 +1,17 @@
 import random
 r = random.randint(1,100)
+count = 0
 while True:
+	count += 1
 	num = input('請猜數字: ')
 	num = int(num)
+	print('猜第 ',count,'次了')	
 	if num == r:
 		print('你猜中了! ')
 		break
 	elif num > r:
 		print('比答案大')
 	elif num < r:
-	    print('比答案小')	
+		print('比答案小')	
+
 			
